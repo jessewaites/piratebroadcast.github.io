26da4187fb021d43751875592314263a8aa7b999
Jesse Waites <jessewaites@Jesses-MacBook-Pro-2.local>
2016-07-21 14:27:02 -0400

    add animation to hamburger menu

--- _site/js/main.js
+++ _site/js/main.js
@@ -104,3 +104,8 @@ $(document).ready(function(){
     app.init();
 });
 
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
